3fa943e38663e08c8889e014ad12f4bdc6f897ca
yfchen123 <61913966+yfchen123@users.noreply.github.com>
2023-08-22 12:28:55 -0700

    Chapter 4 done and uploaded

---
+++ Chapter4_git/Program.cs
@@ -0,0 +1,6 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using static Chapter4.Ch04Ex02PrimeFactorsLib;
+
+String my_str = PrimeFactors(12500);
+Console.WriteLine(my_str);
