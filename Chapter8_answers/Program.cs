ca248355d7b345c8351f8ffdb0cac79a104f340d
yfchen123 <61913966+yfchen123@users.noreply.github.com>
2023-08-24 15:50:32 -0700

    Chapter 8 and Chapter 9 done

---
+++ Chapter8_answers/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Chapter8;
+
+/*Ch08Ex02RegularExpressions new_re = new Ch08Ex02RegularExpressions();
+new_re.RegularExpressions();*/
+
+int my_num = 55;
+Console.WriteLine(my_num.ToWords());
+
+int my_num1 = 25;
+Console.WriteLine(my_num1.ToWords());
+
+int my_num2 = 35;
+Console.WriteLine(my_num2.ToWords());
+
+int my_num3 = 78;
+Console.WriteLine(my_num3.ToWords());
+
+int my_num4 = 124;
+Console.WriteLine(my_num4.ToWords());
+
+int my_num5 = 326;
+Console.WriteLine(my_num5.ToWords());
+
+int my_num6 = 999;
+Console.WriteLine(my_num6.ToWords());
+
+int my_num7 = 1255;
+Console.WriteLine(my_num7.ToWords());
+
+int my_num8 = 12347;
+Console.WriteLine(my_num8.ToWords());
+
+int my_num9 = 665535;
+Console.WriteLine(my_num9.ToWords());
+
+int my_num10 = 1_255_876;
+Console.WriteLine(my_num10.ToWords());
+
+int my_num11 = 12_347_987;
+Console.WriteLine(my_num11.ToWords());
+
+int my_num12 = 678_933_567;
+Console.WriteLine(my_num12.ToWords());
